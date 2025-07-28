fce8e3442f1a2aec01e24c28e65b9d2452446010
pauclus <pauloguzman0709@gmail.com>
2025-07-28 13:17:46 -0500

    Base

---
+++ src/main/java/com/perruno/Identificacion/IdentificacionApplication.java
@@ -0,0 +1,13 @@
+package com.perruno.Identificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentificacionApplication.class, args);
+	}
+
+}
